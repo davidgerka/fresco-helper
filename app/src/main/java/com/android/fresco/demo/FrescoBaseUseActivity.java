c1c2c726d8c51f8887b57f532cfc4027d400fc98
李庆年 <1713923044@qq.com>
2017-05-12 20:18:47 +0800

    1、加入OverScrollView

--- app/src/main/java/com/android/fresco/demo/FrescoBaseUseActivity.java
+++ app/src/main/java/com/android/fresco/demo/FrescoBaseUseActivity.java
@@ -1,14 +1,19 @@
 package com.android.fresco.demo;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.anbetter.log.MLog;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.fresco.helper.ImageLoader;
 import com.facebook.fresco.helper.Phoenix;
+import com.facebook.fresco.helper.listener.IResult;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by android_ls on 16/11/10.
@@ -16,12 +21,35 @@ import com.facebook.fresco.helper.Phoenix;
 
 public class FrescoBaseUseActivity extends AppCompatActivity {
 
+    CircleImageView circleImageView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fresco_base_use);
 
         String url = "http://ww3.sinaimg.cn/large/610dc034jw1f6m4aj83g9j20zk1hcww3.jpg";
+
+        circleImageView = (CircleImageView) findViewById(R.id.sdv_0);
+
+
+        final ImageView imageView = (ImageView) findViewById(R.id.iv_thumbnail);
+        String url1 = "http://ww1.sinaimg.cn/large/610dc034jw1fahy9m7xw0j20u00u042l.jpg";
+        Phoenix.with(this)
+                .setUrl(url1)
+                .setWidth(180)
+                .setHeight(180)
+                .setCircleBitmap(false) // 圆形的
+                .setResult(new IResult<Bitmap>() {
+                    @Override
+                    public void onResult(Bitmap bitmap) {
+                        MLog.i("Thread.currentThread().getId() = " + Thread.currentThread().getId());
+
+                        circleImageView.setImageBitmap(bitmap);
+                    }
+                })
+                .load();
+
 
         // 从网络加载一张图片
         Phoenix.with((SimpleDraweeView)findViewById(R.id.sdv_1)).load(url);
